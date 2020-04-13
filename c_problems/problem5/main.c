e8df25263925345b86b5fd950dae356692d42035
mmfrenkel <megan.frenkel@gmail.com>
2020-04-12 21:13:22 -0400

    fixing indentation

--- c_problems/problem5/main.c
+++ c_problems/problem5/main.c
@@ -3,10 +3,10 @@
 int findPeak(int *, int);
 
 int main(void) {
-	int numElements = 6;
-	int arr[] = {1, 3, 7, 4, 8, 2};
-	int peak = findPeak(arr, numElements);
-	printf("The peak function should return 7. Result is: %d\n", peak);
+    int numElements = 6;
+    int arr[] = {1, 3, 7, 4, 8, 2};
+    int peak = findPeak(arr, numElements);
+    printf("The peak function should return 7. Result is: %d\n", peak);
 }
 
 /*
@@ -17,26 +17,26 @@ int main(void) {
  * all others.
  */
 int findPeak(int *arrPtr, int numElem) {
-	int start = 0;
-	int end = numElem - 1;
+    int start = 0;
+    int end = numElem - 1;
 
-	while (start + 1 < end) {
-		int middle = (start + end) / 2;
-		int valueMiddle = *(arrPtr + middle);
-		int valueLeft = *(arrPtr + middle - 1);
-		int valueRight = *(arrPtr + middle + 1);
-	
-		printf("Middle: %d, Left: %d, Right: %d\n", valueMiddle, valueLeft, valueRight);
-		if (valueMiddle > valueLeft && valueMiddle > valueRight) {
-			return valueMiddle;
-		}
-		else if (valueMiddle < valueLeft) {
-			end = middle - 1;
-		}
-		else {
-			start = middle + 1;
-		}
-	}
-	return *(arrPtr);
+    while (start + 1 < end) {
+        int middle = (start + end) / 2;
+        int valueMiddle = *(arrPtr + middle);
+        int valueLeft = *(arrPtr + middle - 1);
+        int valueRight = *(arrPtr + middle + 1);
+
+        printf("Middle: %d, Left: %d, Right: %d\n", valueMiddle, valueLeft, valueRight);
+        if (valueMiddle > valueLeft && valueMiddle > valueRight) {
+            return valueMiddle;
+        }
+        else if (valueMiddle < valueLeft) {
+            end = middle - 1;
+        }
+        else {
+            start = middle + 1;
+        }
+    }
+    return *(arrPtr);
 }
 
